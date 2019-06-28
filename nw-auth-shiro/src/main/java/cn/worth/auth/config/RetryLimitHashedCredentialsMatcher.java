fdce52491afa03ea0ff15e9449fc4f7b1f433304
chenxiaoqing <chenxiaoqing@qianxx.com>
2019-06-29 02:13:34 +0800

    Revert "提交登陆验证代码"
    This reverts commit 977b54ce

--- nw-auth-shiro/src/main/java/cn/worth/auth/config/RetryLimitHashedCredentialsMatcher.java
+++ nw-auth-shiro/src/main/java/cn/worth/auth/config/RetryLimitHashedCredentialsMatcher.java
@@ -26,10 +26,8 @@ public class RetryLimitHashedCredentialsMatcher extends SimpleCredentialsMatcher
 
     private Cache<String, AtomicInteger> passwordRetryCache;
 
-    private static final String PASSWORD_RETRY_CACHE = ShiroConstant.PASSWORD_RETRY_CACHE;
-
     public RetryLimitHashedCredentialsMatcher(CacheManager cacheManager) {
-        passwordRetryCache = cacheManager.getCache(PASSWORD_RETRY_CACHE);
+        passwordRetryCache = cacheManager.getCache(ShiroConstant.PASSWORD_RETRY_CACHE);
     }
 
     @Override
@@ -37,15 +35,13 @@ public class RetryLimitHashedCredentialsMatcher extends SimpleCredentialsMatcher
         //获取用户名
         String username = (String)token.getPrincipal();
         //获取用户登录次数
-        String retryCacheNameKey = PASSWORD_RETRY_CACHE + username;
-        AtomicInteger retryCount = passwordRetryCache.get(retryCacheNameKey);
+        AtomicInteger retryCount = passwordRetryCache.get(username);
         if (retryCount == null) {
             //如果用户没有登陆过,登陆次数加1 并放入缓存
             retryCount = new AtomicInteger(0);
-            passwordRetryCache.put(retryCacheNameKey, retryCount);
+            passwordRetryCache.put(username, retryCount);
         }
-        int retryTimes = retryCount.intValue();
-
+        int retryTimes = retryCount.incrementAndGet();
         if (retryTimes > 5) {
             //如果用户登陆失败次数大于5次 抛出锁定用户异常  并修改数据库字段
             UserVO userVO = userService.loadUserByUsername(username);
@@ -58,23 +54,33 @@ public class RetryLimitHashedCredentialsMatcher extends SimpleCredentialsMatcher
                 userService.updateById(user);
             }
             log.info("锁定用户" + userVO.getUsername());
-            removeRetryAccountLimitKey(retryCacheNameKey);
             //抛出用户锁定异常
             throw new LockedAccountException();
         }
         //判断用户账号和密码是否正确
         boolean matches = super.doCredentialsMatch(token, info);
         if (matches) {
-            removeRetryAccountLimitKey(retryCacheNameKey);
+            //如果正确,从缓存中将用户登录计数 清除
+            passwordRetryCache.remove(username);
         }
-        retryTimes++;
-        passwordRetryCache.put(retryCacheNameKey, new AtomicInteger(retryTimes));
         return matches;
     }
 
-    private void removeRetryAccountLimitKey(String retryCacheNameKey) {
-        //如果正确,从缓存中将用户登录计数 清除
-        passwordRetryCache.remove(retryCacheNameKey);
+    /**
+     * 根据用户名 解锁用户
+     * @param username
+     * @return
+     */
+    public void unlockAccount(String username){
+        UserVO userVO = userService.loadUserByUsername(username);
+        if (userVO != null){
+            User user = new User();
+            //修改数据库的状态字段为锁定
+            user.setId(userVO.getId());
+            user.setState(0);
+            userService.updateById(user);
+            passwordRetryCache.remove(username);
+        }
     }
 
 }
